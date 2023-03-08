a0df2244599281b2d9a25e872cfa3f92c35a02ad
Evan <evanlowell01@gmail.com>
2023-03-08 18:42:23 -0500

    Creates additional elements and adds newItem function

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-function newImage (x, y, lt, bt) {
-    let x = document.createElement('img')
-    x.src = y
-    x.style.position = 'fixed'
-    x.style.left = lt
-    x.style.bottom = bt
-    document.body.append(x)
+function newImage (pth, lt, bt) {
+    let object = document.createElement('img')
+    object.src = pth
+    object.style.position = 'fixed'
+    object.style.left = lt + 'px'
+    object.style.bottom = bt + 'px'
+    document.body.append(object)
+    return object
 }
 
-newImage(greenCharacter, 'assets/green-character.gif', '100px', '100px');
+newImage('assets/green-character.gif', 100, 100);
 
-newImage(pineTree, 'assets/pine-tree.png', '450px', '200px');
+newImage('assets/pine-tree.png', 450, 200);
+
+newImage('assets/pillar.png', 350, 100);
+
+newImage('assets/pine-tree.png', 450, 200);
+
+newImage('assets/crate.png', 150, 200);
+
+newImage('assets/well.png', 500, 425);
+
+function newItem(pth, lt, bt){
+    let object = newImage(pth, lt, bt)
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+
+newItem('assets/sword.png', 500, 405);
+
+newItem('assets/shield.png', 165, 185);
+
+newItem('assets/staff.png', 600, 100);
+
 
 /*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
