1e9a96f85cc271c8c7e03d80c6a7ac74515d9c1d
Evan <evanlowell01@gmail.com>
2023-03-08 18:30:13 -0500

    Adds newImage function

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-let greenCharacter = document.createElement('img')
+function newImage (x, y, lt, bt) {
+    let x = document.createElement('img')
+    x.src = y
+    x.style.position = 'fixed'
+    x.style.left = lt
+    x.style.bottom = bt
+    document.body.append(x)
+}
+
+newImage(greenCharacter, 'assets/green-character.gif', '100px', '100px');
+
+newImage(pineTree, 'assets/pine-tree.png', '450px', '200px');
+
+/*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
@@ -10,4 +23,5 @@ pineTree.src = 'assets/pine-tree.png'
 pineTree.style.position = 'fixed'
 pineTree.style.left = '450px'
 pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+document.body.append(pineTree)*/
+
